37b85753fcc0038dcd0a03924cbceefab39da73c
gustavo807 <galberto807@gmail.com>
2019-11-18 20:30:41 -0600

    Updates

---
+++ precache-manifest.d4241532ce47531e76fa783b536f6b64.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab51989e78224423212d65e084775d20",
+    "url": "/simple-react-router/index.html"
+  },
+  {
+    "revision": "690f17e0b8733e7ddf5a",
+    "url": "/simple-react-router/static/js/2.cc8b6139.chunk.js"
+  },
+  {
+    "revision": "0a1cae8c187ea1156ea0",
+    "url": "/simple-react-router/static/js/main.ef2d4f97.chunk.js"
+  },
+  {
+    "revision": "120c58f3267dbe2a256d",
+    "url": "/simple-react-router/static/js/runtime-main.cb4f6549.js"
+  }
+]);
