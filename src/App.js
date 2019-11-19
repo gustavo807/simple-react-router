80cefc3ca0b0dc7b2627948fd74b10c00e7e1aa7
gustavo807 <galberto807@gmail.com>
2019-11-18 20:29:39 -0600

    Added changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import Posts from './Posts';
 import Post from './Post';
+
 function App(){
 
     return(
